ecafc6382cabe5d2b45f131ac6bc467d6dcde4af
mccartnp <nicholasmccarty252@gmail.com>
2023-10-23 12:26:03 -0400

    Added a time tracker for rcv_msg

--- rcv_msg.cpp
+++ rcv_msg.cpp
@@ -4,8 +4,12 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/stat.h>
+#include <fstream>
+#include <chrono>
 
 int main() {
+    
+    std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now(); 
     const int msqid =
         msgget(123456, S_IRUSR);
     // Check to ensure msqid is not ­‐1!
@@ -13,6 +17,16 @@ int main() {
     while (msgrcv(msqid, &msg, MSG_SIZE, MSG_TYPE, 0) != -1) {
         std::cout << msg.data
             << std::endl;
+
     }
+    
+    std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now(); // Record end time
+    std::chrono::duration<double> elapsedTime = endTime - startTime;
+
+    std::ofstream outputFile("data1.txt");
+    outputFile << "Elapsed Time (seconds)\n";
+    outputFile << elapsedTime.count() << "\n";
+    outputFile.close();
+
     return 0;
 }
